087d2cec846ba3cc774c3e45a32b9c9706d81559
AndriiNyzhnyk1994 <prostoandrew1994@gmail.com>
2020-06-21 00:06:39 +0300

    add new component (Header)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+      <span>Soon there will be a social network or something like that</span>
     </div>
   );
+}
+
+function Header() {
+    return (
+        <div>Header</div>
+    )
 }
 
 export default App;
