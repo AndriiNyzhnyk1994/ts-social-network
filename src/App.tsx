247426aabae1a1929a083b3b08645eab8c4e045c
AndriiNyzhnyk1994 <prostoandrew1994@gmail.com>
2020-07-01 13:36:24 +0300

    Revert "Bump websocket-extensions from 0.1.3 to 0.1.4"
    This reverts commit e9bf26cc0fc41b150571e0e58403c2e225bae179.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,55 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <Header/>
-      <span>Soon there will be a social network or something like that</span>
-    </div>
-  );
-}
-
-function Header() {
     return (
-        <div>Header</div>
-    )
+        <div className="app-wrapper">
+            <header className='header'>
+                <img
+                    src="https://lh3.googleusercontent.com/proxy/9uVLrNPurnGnksGQGE8zeDHvVV5OxF4ts9W6E8_lVJsGyEw_4XkGIqmzAXJy126UihbK1Tgi_ZN9J4pFdYsAg5tc39Kbt3W2FtY8wtDJXe43muSI8zVATA"
+                    alt=""/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/433f9063-3d2f-4d4e-84a8-b5d676ac0f51/da8i24h-ef27f89c-b496-46db-af13-a1e959548c42.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNDMzZjkwNjMtM2QyZi00ZDRlLTg0YTgtYjVkNjc2YWMwZjUxXC9kYThpMjRoLWVmMjdmODljLWI0OTYtNDZkYi1hZjEzLWExZTk1OTU0OGM0Mi5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.jwfXb6A3nrI26mxkunV-hksCiSp5akL3z6BQevus1us"/>
+                </div>
+                <div>
+                    avatar + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            Post 1
+                        </div>
+                        <div>
+                            Post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
