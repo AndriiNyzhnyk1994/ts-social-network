230802d32b3db63c3cf9a09a5121299f75914a60
AndriiNyzhnyk1994 <prostoandrew1994@gmail.com>
2020-07-18 19:16:00 +0300

    1. Add map method to:    dialogsElements    messagesElements    postsElements

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -24,7 +24,8 @@ const Message = (props: MessageType) => {
 
 
 const Dialogs = (props: DialogItemType) => {
-    let dialogsData = [
+    //DATA
+    let dialogs = [
         {id: '1', name: 'Vlad'},
         {id: '2', name: 'Vladick'},
         {id: '3', name: 'Dad'},
@@ -32,7 +33,7 @@ const Dialogs = (props: DialogItemType) => {
         {id: '5', name: 'Vladislav'},
         {id: '6', name: 'Roman'}
     ];
-    let messagesData = [
+    let messages = [
         {id: '1', message: 'Hi'},
         {id: '2', message: 'How are you'},
         {id: '3', message: 'You are adopted'},
@@ -41,15 +42,22 @@ const Dialogs = (props: DialogItemType) => {
         {id: '6', message: "You bastard!!! Give back my mom\'s chain!!!"}
     ];
 
+
+    let dialogsElements = dialogs
+        .map( d => <DialogItem name={d.name} id={d.id}/> );
+
+    let messagesElements = messages
+        .map( m => <Message message={m.message}/> );
+
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
+                {messagesElements}
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -4,13 +4,18 @@ import Post from "./Post/Post";
 import {Button} from "@material-ui/core";
 
 
+
+
 const MyPosts = () => {
 
-    let postsData = [
+    let posts = [
         {id: '1', message: 'Hi, I\'m the real Ryan Gosling. Please send me money for a new movie. I will return twice as much money. Here are my bank account number: 5585 5412 3365 8187.\n' + 'P.S. Account holder\'s name: Sergey Kovalenko (don\'t worry, just my account was blocked).',
             likesCount: 12},
         {id: '2', message: 'So that?? Will you send money ??? I`m really needed', likesCount: 10},
+        {id: '2', message: 'Send me money and I\'ll tell on my twitter that you are a cool man ;)', likesCount: 9},
     ];
+
+    let postsElements = posts.map( p => <Post message={p.message} likesCount={p.likesCount}/> )
 
     return (
         <div className={s.postsBlock}>
@@ -24,14 +29,7 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post
-                    message={postsData[0].message}
-                    likesCount={postsData[0].likesCount}
-                />
-                <Post
-                    message={postsData[1].message}
-                    likesCount={postsData[1].likesCount}
-                />
+                {postsElements}
             </div>
         </div>
     )
